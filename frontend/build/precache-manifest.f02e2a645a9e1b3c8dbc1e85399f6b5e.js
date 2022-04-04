0fd67318b8a538f1676482a835c5e1b51b0be1d2
Vasa <vasa.amadea@gmail.com>
2022-04-04 17:26:56 +1000

    small changes

---
+++ frontend/build/precache-manifest.f02e2a645a9e1b3c8dbc1e85399f6b5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0b6c7f304302d22f6fb51c855e32d7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47f60a7e0a7b7ef0a439",
+    "url": "/static/css/main.be810e76.chunk.css"
+  },
+  {
+    "revision": "340579ac62475c29a50e",
+    "url": "/static/js/2.e7cf4643.chunk.js"
+  },
+  {
+    "revision": "6a3fd5f48a97c7577e9c3ede94b4a5b5",
+    "url": "/static/js/2.e7cf4643.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47f60a7e0a7b7ef0a439",
+    "url": "/static/js/main.0b750b39.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "8e6f5ee33463bb5796ae929b0a586635",
+    "url": "/static/media/logo.8e6f5ee3.png"
+  }
+]);
